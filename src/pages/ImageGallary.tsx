c46c3740b52bf9a989a9097620a3514953590ed8
unknown <nazmulhosenm668@gmail.com>
2023-11-05 23:42:19 +0600

    almost done

--- src/pages/ImageGallary.tsx
+++ src/pages/ImageGallary.tsx
@@ -1,34 +1,10 @@
-import img1 from "../images/image-1.webp";
-import img10 from "../images/image-10.jpeg";
-import img11 from "../images/image-11.jpeg";
-import img2 from "../images/image-2.webp";
-import img3 from "../images/image-3.webp";
-import img4 from "../images/image-4.webp";
-import img5 from "../images/image-5.webp";
-import img6 from "../images/image-6.webp";
-import "../styles/imageGallary.css";
-import img7 from "../images/image-7.webp";
-import img8 from "../images/image-8.webp";
-import img9 from "../images/image-9.webp";
 import { useState } from "react";
-
-const data: { id: number; img: string }[] = [
-  { id: 1, img: img11 },
-  { id: 2, img: img2 },
-  { id: 3, img: img3 },
-  { id: 4, img: img5 },
-  { id: 5, img: img4 },
-  { id: 6, img: img6 },
-  { id: 7, img: img7 },
-  { id: 8, img: img8 },
-  { id: 9, img: img9 },
-  { id: 10, img: img10 },
-  { id: 11, img: img1 },
-];
+import { data } from "../utils/images";
 
 function ImageGallary() {
   const [images, setImages] = useState(data);
   const [selectImg, setSelectImg] = useState<number[]>([]);
+  const [draggedImage, setDraggedImage] = useState<any>(null);
 
   const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const checked = e.target.checked;
@@ -52,26 +28,22 @@ function ImageGallary() {
     setSelectImg([]);
   };
 
-  // dragable
+  const resetSelectImageHandler = () => {
+    setSelectImg([]);
+  };
+
   // Handle drag start
-  const [dragging, setDragging] = useState(false);
-  const [draggedImage, setDraggedImage] = useState<any>(null);
-  const [draggedIndex, setDraggedIndex] = useState(null);
   const handleDragStart = (image: any) => {
-    setDragging(true);
     setDraggedImage(image);
   };
 
   // Handle drag over
   const handleDragOver = (e: any) => {
     e.preventDefault();
-    e?.target?.children[0]?.alt && setDraggedIndex(e?.target?.children[0]?.alt);
   };
 
   // Handle drop image
   const handleDrop = (targetIndex: any) => {
-    setDragging(false);
-
     if (draggedImage) {
       const updatedImages = images.filter(
         (image) => image.id !== draggedImage.id
@@ -89,7 +61,7 @@ function ImageGallary() {
         <div className="gallary-header">
           <label className="checkbox">
             {selectImg.length} Files Selected
-            <input type="checkbox" checked />
+            <input onClick={resetSelectImageHandler} type="checkbox" checked />
             <span className="checkmark"></span>
           </label>
 
@@ -120,8 +92,14 @@ function ImageGallary() {
                 onChange={handleCheckboxChange}
                 type="checkbox"
                 value={item.id}
+                checked={selectImg.includes(item.id) ? true : false}
               />
-              <span className="img-checkmark"></span>
+              <span
+                className="img-checkmark"
+                style={{
+                  display: selectImg.includes(item.id) ? "block" : "none",
+                }}
+              ></span>
             </label>
           );
         })}
---
+++ src/utils/images.ts
@@ -0,0 +1,27 @@
+
+import img1 from "../images/image-1.webp";
+import img10 from "../images/image-10.jpeg";
+import img11 from "../images/image-11.jpeg";
+import img2 from "../images/image-2.webp";
+import img3 from "../images/image-3.webp";
+import img4 from "../images/image-4.webp";
+import img5 from "../images/image-5.webp";
+import img6 from "../images/image-6.webp";
+import img7 from "../images/image-7.webp";
+import img8 from "../images/image-8.webp";
+import img9 from "../images/image-9.webp";
+import "../styles/imageGallary.css";
+
+export const data: { id: number; img: string }[] = [
+  { id: 1, img: img11 },
+  { id: 2, img: img2 },
+  { id: 3, img: img3 },
+  { id: 4, img: img5 },
+  { id: 5, img: img4 },
+  { id: 6, img: img6 },
+  { id: 7, img: img7 },
+  { id: 8, img: img8 },
+  { id: 9, img: img9 },
+  { id: 10, img: img10 },
+  { id: 11, img: img1 },
+];
